70fb7970f27a313ee93c6a7f65abe45cac65820e
wanggang <lambdaframework@icloud.com>
2023-06-14 15:47:18 +0800

    change

--- lambda-framework-common/src/main/java/org/lambda/framework/common/util/sample/BeanPlasticityUtil.java
+++ lambda-framework-common/src/main/java/org/lambda/framework/common/util/sample/BeanPlasticityUtil.java
@@ -27,30 +27,6 @@ public class BeanPlasticityUtil {
             throw new EventException(ES_COMMON_021);
         }
     }
-
-    static {
-        ConvertUtils.register(new Converter() {
-            @Override
-            public Object convert(Class type, Object value) {
-                if (value == null) {
-                    return null;
-                }
-                if (!(value instanceof String)) {
-                    throw new EventException(ES_COMMON_022);
-                }
-                if (StringUtils.isBlank((String) value)) {
-                    throw new EventException(ES_COMMON_023);
-                }
-
-                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-                try {
-                    return df.parse((String) value);
-                } catch (ParseException e) {
-                    throw new EventException(ES_COMMON_024);
-                }
-            }
-        }, java.util.Date.class);
-    }
 }
 
 
