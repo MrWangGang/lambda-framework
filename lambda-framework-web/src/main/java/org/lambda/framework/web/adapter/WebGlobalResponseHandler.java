3cd5d1f0ce0289003194a5953ab0026776a329f4
wanggang <lambdaframework@icloud.com>
2023-06-15 03:32:49 +0800

    change

---
+++ lambda-framework-web/src/main/java/org/lambda/framework/web/adapter/WebGlobalResponseHandler.java
@@ -0,0 +1,75 @@
+package org.lambda.framework.web.adapter;
+
+import org.lambda.framework.common.exception.EventException;
+import org.lambda.framework.common.templete.ResponseTemplete;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.ReactiveAdapterRegistry;
+import org.springframework.http.codec.HttpMessageWriter;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.HandlerResult;
+import org.springframework.web.reactive.accept.RequestedContentTypeResolver;
+import org.springframework.web.reactive.result.method.annotation.ResponseBodyResultHandler;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.lambda.framework.web.enums.WebExceptionEnum.ES_WEB_001;
+import static org.lambda.framework.web.enums.WebExceptionEnum.ES_WEB_002;
+
+@Configuration
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+public class WebGlobalResponseHandler  {
+
+    private static Mono<ResponseTemplete> methodForParams() {
+        return Mono.empty();
+    }
+    private static MethodParameter METHOD_PARAMETER;
+
+    static {
+        try {
+            // 获得 METHOD_PARAMETER_MONO_COMMON_RESULT 。其中 -1 表示 `#methodForParams()` 方法的返回值
+            METHOD_PARAMETER = new MethodParameter(
+                    WebGlobalResponseHandler.class.getDeclaredMethod("methodForParams"), -1);
+        } catch (NoSuchMethodException e) {
+            throw new EventException(ES_WEB_001);
+        }
+    }
+
+    @Bean
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public ResponseBodyResultHandler responseWrapper(ServerCodecConfigurer serverCodecConfigurer,
+                                                    RequestedContentTypeResolver requestedContentTypeResolver) {
+        return new ResponseBodyResultHandler(serverCodecConfigurer.getWriters(), requestedContentTypeResolver) {
+            @Override
+            public boolean supports(HandlerResult result) {
+                return true;
+            }
+            @Override
+            public Mono<Void> handleResult(ServerWebExchange exchange, HandlerResult result) {
+                Object returnValue = result.getReturnValue();
+                Object body;
+                // <1.1>  处理返回结果为 Mono 的情况
+                if (returnValue instanceof Mono) {
+                    body = ((Mono<Object>) result.getReturnValue())
+                            .map(e-> new ResponseTemplete(e))
+                            .defaultIfEmpty(new ResponseTemplete());
+                    //  <1.2> 处理返回结果为 Flux 的情况
+                } else if (returnValue instanceof Flux) {
+                    body = ((Flux<Object>) result.getReturnValue())
+                            .collectList()
+                            .map(e-> new ResponseTemplete(e))
+                            .defaultIfEmpty(new ResponseTemplete());
+                    //  <1.3> 处理结果为其它类型
+                } else {
+                    //不允许其他类型返回
+                    throw new EventException(ES_WEB_002);
+                }
+                return writeBody(body, METHOD_PARAMETER, exchange);
+            }
+        };
+    }
+}
--- lambda-framework-web/src/main/java/org/lambda/framework/web/enums/WebExceptionEnum.java
+++ lambda-framework-web/src/main/java/org/lambda/framework/web/enums/WebExceptionEnum.java
@@ -7,7 +7,11 @@ import org.lambda.framework.common.exception.ExceptionEnumFunction;
 public enum WebExceptionEnum implements ExceptionEnumFunction {
 
     //系统异常-普通异常 0-99
-    ES_WEB_000("ES_WEB_000","系统错误");
+    ES_WEB_000("ES_WEB_000","系统错误"),
+    ES_WEB_001("ES_WEB_001","[static][获取 METHOD_PARAMETER 时，找不都方法"),
+    ES_WEB_002("ES_WEB_002","response不允许返回其他类型");
+
+
     // 成员变量
 
     @Getter
