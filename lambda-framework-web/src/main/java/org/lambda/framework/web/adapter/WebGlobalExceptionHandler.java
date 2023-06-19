b0e0035fd90533bbfb8a2ddd4226b96c8f94ab77
wanggang <lambdaframework@icloud.com>
2023-06-19 10:29:40 +0800

    增加解决跨域配置

--- lambda-framework-web/src/main/java/org/lambda/framework/web/adapter/WebGlobalExceptionHandler.java
+++ lambda-framework-web/src/main/java/org/lambda/framework/web/adapter/WebGlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
