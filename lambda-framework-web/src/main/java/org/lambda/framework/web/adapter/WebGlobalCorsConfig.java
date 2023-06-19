0e2fb8d77ac0ece00082d95927d98eb908c93ceb
wanggang <lambdaframework@icloud.com>
2023-06-19 15:31:51 +0800

    增加解决跨域配置

--- lambda-framework-web/src/main/java/org/lambda/framework/web/adapter/WebGlobalCorsConfig.java
+++ lambda-framework-web/src/main/java/org/lambda/framework/web/adapter/WebGlobalCorsConfig.java
@@ -12,13 +12,14 @@ public class WebGlobalCorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("*"); // 允许的源（域），可以设置具体的域名
         corsConfig.addAllowedMethod(HttpMethod.GET);
         corsConfig.addAllowedMethod(HttpMethod.POST);
         corsConfig.addAllowedMethod(HttpMethod.PUT);
         corsConfig.addAllowedMethod(HttpMethod.DELETE);
         corsConfig.addAllowedMethod(HttpMethod.PATCH);
-        corsConfig.addAllowedHeader("*"); // 允许的请求头
+        corsConfig.addAllowedOrigin("*"); // 允许的源（域），可以设置具体的域名
+        corsConfig.addAllowedHeader("Content-Type");
+        corsConfig.addAllowedHeader("Auth-Token");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
 
