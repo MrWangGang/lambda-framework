6e700e6ed97904ac78141f8234140b0cdcf13501
wanggang <lambdaframework@icloud.com>
2023-06-21 16:21:32 +0800

    增加解决跨域配置

---
+++ lambda-framework-web/src/main/java/org/lambda/framework/web/adapter/WebJacksonGlobalConfig.java
@@ -0,0 +1,32 @@
+package org.lambda.framework.web.adapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class WebJacksonGlobalConfig {
+    @Bean
+    public ObjectMapper objectMapper(ObjectMapper globalObjectMapper) {
+        return globalObjectMapper.copy();
+    }
+
+    @Bean
+    public ObjectMapper globalObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        objectMapper.registerModule(javaTimeModule);
+
+        return objectMapper;
+    }
+}
