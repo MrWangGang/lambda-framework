3756b017d3b38ffb089a739d9ee2e2dea4559900
wanggang <lambdaframework@icloud.com>
2023-06-27 17:39:42 +0800

    增加解决跨域配置

--- lambda-framework-rpc/src/main/java/org/lambda/framework/rpc/RpcPostProcessor.java
+++ lambda-framework-rpc/src/main/java/org/lambda/framework/rpc/RpcPostProcessor.java
@@ -1,30 +1,10 @@
 package org.lambda.framework.rpc;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.Resource;
 import org.lambda.framework.common.exception.EventException;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerClientRequestTransformer;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactiveLoadBalancer;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.ResourceLoaderAware;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
@@ -36,16 +16,11 @@ import org.springframework.web.reactive.function.client.support.WebClientAdapter
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
-import java.util.List;
-
 import static org.lambda.framework.rpc.enums.RpcExceptionEnum.ES_RPC_000;
 
 @Component
 public class RpcPostProcessor implements ResourceLoaderAware,BeanFactoryPostProcessor  {
     private ResourceLoader resourceLoader;
-    @Resource
-    private ReactorLoadBalancerExchangeFilterFunction reactorLoadBalancerExchangeFilterFunction;
-
     @Override
     public void setResourceLoader(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
@@ -71,7 +46,8 @@ public class RpcPostProcessor implements ResourceLoaderAware,BeanFactoryPostProc
                     //利用类的全限定名通过Class.forName获取class对象并利用http服务的代理工厂创建出代理对象
                     Object client = factory.createClient(Class.forName(metadataReader.getClassMetadata().getClassName()));
                     //将创建出来的代理对象放到io容器当中
-                    beanFactory.registerSingleton(metadataReader.getClassMetadata().getClassName(), client);
+                    String className = metadataReader.getClassMetadata().getClassName();
+                    beanFactory.registerSingleton(className, client);
                 }
             }
         } catch (Exception e) {
