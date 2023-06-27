7bf612b247c397a1cbb9d0ccfd8a7e500a0bb7b7
wanggang <lambdaframework@icloud.com>
2023-06-27 16:24:36 +0800

    增加解决跨域配置

--- lambda-framework-common/src/main/java/org/lambda/framework/common/util/sample/BeanPlasticityUtil.java
+++ lambda-framework-common/src/main/java/org/lambda/framework/common/util/sample/BeanPlasticityUtil.java
@@ -1,12 +1,12 @@
 package org.lambda.framework.common.util.sample;
 
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
 import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.beanutils.BeanUtilsBean;
 import org.lambda.framework.common.exception.EventException;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 import static org.lambda.framework.common.enums.CommonExceptionEnum.ES_COMMON_021;
 
@@ -19,29 +19,17 @@ public class BeanPlasticityUtil {
             T result = t.newInstance();
             BeanUtils.copyProperties(result, orig);
             return result;
-        } catch (IllegalAccessException e) {
-            throw new EventException(ES_COMMON_021);
-        } catch (InstantiationException e) {
-            throw new EventException(ES_COMMON_021);
-        } catch (InvocationTargetException e) {
-            throw new EventException(ES_COMMON_021);
-        } catch (NoSuchMethodException e) {
+        } catch (Exception e) {
             throw new EventException(ES_COMMON_021);
         }
     }
 
-    //target2非空属性复置到target1
-    public static void copy(Object target1, Object target2) {
+    public static  void copy(Object target1, Object target2) {
         try {
-            BeanUtilsBean beanUtilsBean = BeanUtilsBean.getInstance();
-            // 自定义属性复制的行为
-            beanUtilsBean.getConvertUtils().register(false, true, 0);
-            // 复制target2对象的非空属性到target1对象
-            beanUtilsBean.copyProperties(target1, target2);
-        } catch (IllegalAccessException e) {
+            BeanUtil.copyProperties(target1, target2, CopyOptions.create().setIgnoreNullValue(true));
+        }catch (Exception e){
             throw new EventException(ES_COMMON_021);
-        } catch (InvocationTargetException e) {
-            throw new EventException(ES_COMMON_021);
+
         }
     }
 }
