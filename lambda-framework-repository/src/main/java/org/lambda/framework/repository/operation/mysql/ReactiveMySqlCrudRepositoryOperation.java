d451754496ab01fad74ba8038c215118fdf34861
MrWangGang <lambdaframework@icloud.com>
2023-07-07 14:46:52 +0800

    增加解决跨域配置

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/mysql/ReactiveMySqlCrudRepositoryOperation.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/mysql/ReactiveMySqlCrudRepositoryOperation.java
@@ -1,6 +1,6 @@
 package org.lambda.framework.repository.operation.mysql;
 
 
-public interface ReactiveMySqlCrudRepositoryOperation<Entity,IdType> extends ReactiveMySqlCoreRepositoryOperation<Entity,Long>, ReactiveMySqlPagingRepositoryOperation<Entity> {
+public interface ReactiveMySqlCrudRepositoryOperation<Entity,IdType> extends ReactiveMySqlCoreRepositoryOperation<Entity,IdType>, ReactiveMySqlPagingRepositoryOperation<Entity> {
 
 }
