48aa5e50a3613baae19b65c12ccf1f13baa34b68
MrWangGang <lambdaframework@icloud.com>
2023-08-08 08:36:01 +0800

    增加了rpc注解 增加了rpc的自动化配置 解决了rpc bean的注入问题 增加了rpc对注册中心的支持和负载均衡

--- lambda-framework-security/src/main/java/org/lambda/framework/security/manger/SecurityAutzManager.java
+++ lambda-framework-security/src/main/java/org/lambda/framework/security/manger/SecurityAutzManager.java
@@ -31,17 +31,6 @@ public abstract class SecurityAutzManager implements ReactiveAuthorizationManage
     @Resource(name = "securityAutzRedisOperation")
     private ReactiveRedisOperation securityAutzRedisOperation;
 
-
-    private String urlAutz;
-    @Value("${lambda.security.url_autz:"+ SecurityContract.LAMBDA_SECURITY_URL_AUTZ_ALL+"}")
-    public void setUrlAutz(String urlAutz) {
-        if(SecurityContract.LAMBDA_SECURITY_URL_AUTZ_ALL.equals(urlAutz) || SecurityContract.LAMBDA_SECURITY_URL_AUTZ_MAPPING.equals(urlAutz)) {
-            this.urlAutz = urlAutz;
-        }else {
-            throw new EventException(SecurityExceptionEnum.ES_SECURITY_010);
-        }
-    }
-
     @Override
     public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, AuthorizationContext authorizationContext) {
 
@@ -53,21 +42,7 @@ public abstract class SecurityAutzManager implements ReactiveAuthorizationManage
                         .onErrorResume(e->Mono.just(SecurityContract.LAMBDA_SECURITY_EMPTY_STR))
                         .defaultIfEmpty(SecurityContract.LAMBDA_SECURITY_EMPTY_STR)
                         .flatMap(currentPathAutzTree->{
-                            if(StringUtils.isBlank(currentPathAutzTree.toString())){
-                                //如果路径权限树为空
-                                if(SecurityContract.LAMBDA_SECURITY_URL_AUTZ_ALL.equals(urlAutz)){
-                                    //配置了所有的经过认证都需要授权
-                                    return Mono.error(new EventException(SecurityExceptionEnum.ES_SECURITY_001));
-                                }
-                                if(SecurityContract.LAMBDA_SECURITY_URL_AUTZ_MAPPING.equals(urlAutz)){
-                                    //配置了只有映射的URL经过认证才需要授权
-                                    return Mono.just(currentPathAutzTree.toString());
-                                }
-                                return Mono.error(new EventException(SecurityExceptionEnum.ES_SECURITY_010));
-                            }
-                            return Mono.just(currentPathAutzTree.toString());
-                        }).flatMap(currentPathAutzTree->{
-                            if(!verify(currentPathAutzTree,auth.getPrincipal().toString()))return Mono.error(new EventException(SecurityExceptionEnum.ES_SECURITY_001));
+                            if(!verify(currentPathAutzTree.toString(),auth.getPrincipal().toString()))return Mono.error(new EventException(SecurityExceptionEnum.ES_SECURITY_001));
                             return Mono.just(new AuthorizationDecision(true));
                     });
          });
