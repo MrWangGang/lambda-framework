94be04dd06b2e05136b0c0c356b6dea828e290f3
MrWangGang <lambdaframework@icloud.com>
2023-08-23 14:13:51 +0800

    测试gitee与github 联通

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/ReactiveUnifyPagingRepositoryOperation.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/ReactiveUnifyPagingRepositoryOperation.java
@@ -7,11 +7,11 @@ import static java.lang.Math.ceil;
 public interface ReactiveUnifyPagingRepositoryOperation {
 
 
-    default <Condition,Entity>Mono<Paged<Entity>> find(Integer page, Integer size,Condition condition,UnifyPagingOperation operation){
-        return operation.<Condition,Entity>count(page,size,condition).switchIfEmpty(Mono.just(Integer.valueOf(0))).flatMap(e->{
+    default <Condition,Entity>Mono<Paged<Entity>> find(Integer page, Integer size,Condition condition,UnifyPagingOperation<Entity> operation){
+        return operation.count().switchIfEmpty(Mono.just(Integer.valueOf(0))).flatMap(e->{
             return Mono.just(Paged.<Entity>builder().page(page).size(size).total(e).pages(((int)ceil(e/size))).build());
         }).flatMap(e->{
-            return operation.<Condition,Entity>query(page,size,condition).collectList().flatMap(records->{
+            return operation.<Entity>query().collectList().flatMap(records->{
                 e.setRecords(records);
                 return Mono.just(e);
             });
--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/UnifyPagingOperation.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/UnifyPagingOperation.java
@@ -3,8 +3,8 @@ package org.lambda.framework.repository.operation;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface UnifyPagingOperation {
-    public <Condition,Entity>Mono<Integer> count(Integer page,Integer size,Condition condition);
+public interface UnifyPagingOperation<Entity> {
+    public Mono<Integer> count();
 
-    public <Condition,Entity>Flux<Entity> query(Integer page,Integer size,Condition condition);
+    public Flux<Entity> query();
 }
