155a618b4f430da523d7ec3d55829458f7ac4ef3
MrWangGang <lambdaframework@icloud.com>
2023-08-23 11:11:29 +0800

    测试gitee与github 联通

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/UnifyPagingOperation.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/UnifyPagingOperation.java
@@ -3,8 +3,8 @@ package org.lambda.framework.repository.operation;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface UnifyPagingOperation<Entity> {
-    public <Condition>Mono<Integer> count(Integer page,Integer size,Condition condition);
+public interface UnifyPagingOperation {
+    public <Condition,Entity>Mono<Integer> count(Integer page,Integer size,Condition condition);
 
-    public <Condition>Flux<Entity> query(Integer page,Integer size,Condition condition);
+    public <Condition,Entity>Flux<Entity> query(Integer page,Integer size,Condition condition);
 }
