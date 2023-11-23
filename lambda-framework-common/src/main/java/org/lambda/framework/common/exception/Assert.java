a77c1e31bcb2a9c6ad8b02693f9ce132514e4566
wanggang <userbean@outlook.com>
2023-11-23 16:19:04 +0800

    增加合规组件，从 controller层->service->dao ,以及组织架构树的构建和遍历树 都走合规组件，便于代码的规范统一

--- lambda-framework-common/src/main/java/org/lambda/framework/common/exception/Assert.java
+++ lambda-framework-common/src/main/java/org/lambda/framework/common/exception/Assert.java
@@ -1,7 +1,14 @@
 package org.lambda.framework.common.exception;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Assert {
     public static void verify(Object object,ExceptionEnumFunction exceptionEnumFunction){
+        if(object instanceof String){
+            if(StringUtils.isBlank((String) object)){
+                throw new EventException(exceptionEnumFunction);
+            }
+        }
         if(object == null)throw new EventException(exceptionEnumFunction);
     }
 }
