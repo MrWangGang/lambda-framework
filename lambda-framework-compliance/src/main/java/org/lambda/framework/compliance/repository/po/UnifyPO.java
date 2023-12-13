736bb218312a3226d9efe4d37665b34117d543f2
wanggang <userbean@outlook.com>
2023-12-13 14:40:38 +0800

    过期时间改成30天

--- lambda-framework-compliance/src/main/java/org/lambda/framework/compliance/repository/po/UnifyPO.java
+++ lambda-framework-compliance/src/main/java/org/lambda/framework/compliance/repository/po/UnifyPO.java
@@ -1,5 +1,6 @@
 package org.lambda.framework.compliance.repository.po;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,15 +8,16 @@ import java.time.LocalDateTime;
 @Data
 public class UnifyPO {
 
+    @JsonIgnore
     private LocalDateTime createTime;
-
+    @JsonIgnore
     private LocalDateTime updateTime;
-
+    @JsonIgnore
     private Long creatorId;
-
+    @JsonIgnore
     private Long updaterId;
-
+    @JsonIgnore
     private String creatorName;
-
+    @JsonIgnore
     private String updaterName;
 }
