95efac0feda136b6b74162bc22b08c78a00750aa
wanggang <userbean@outlook.com>
2023-12-21 15:06:36 +0800

    新增 assert方法对list的校验

--- lambda-framework-common/src/main/java/org/lambda/framework/common/exception/Assert.java
+++ lambda-framework-common/src/main/java/org/lambda/framework/common/exception/Assert.java
@@ -2,10 +2,24 @@ package org.lambda.framework.common.exception;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Collection;
+import java.util.List;
+
 public class Assert {
     public static void verify(Object object,ExceptionEnumFunction exceptionEnumFunction){
         if(object instanceof String){
             if(StringUtils.isBlank((String) object)){
+                throw new EventException(exceptionEnumFunction);
+            }
+        }
+        if(object instanceof List){
+            if(object == null){
+                throw new EventException(exceptionEnumFunction);
+            }
+            if(((List)object).size() == 0){
+                throw new EventException(exceptionEnumFunction);
+            }
+            if(((List)object).get(0) ==  null){
                 throw new EventException(exceptionEnumFunction);
             }
         }
