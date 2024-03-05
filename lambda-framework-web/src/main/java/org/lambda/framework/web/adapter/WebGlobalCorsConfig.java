5d97b953082bc9b0adfce0247f4a4f1dd77b76c1
wanggang <userbean@outlook.com>
2024-03-05 09:21:13 +0800

    跨域现在针对所有的

--- lambda-framework-web/src/main/java/org/lambda/framework/web/adapter/WebGlobalCorsConfig.java
+++ lambda-framework-web/src/main/java/org/lambda/framework/web/adapter/WebGlobalCorsConfig.java
@@ -6,34 +6,21 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 public class WebGlobalCorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
-        UrlBasedCorsConfigurationSource configSource =
-                new UrlBasedCorsConfigurationSource();
-
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        config.addAllowedHeader("User-Agent");
-        config.addAllowedHeader("Cache-Control");
-        config.addAllowedHeader("X-Requested-With");
-        config.addAllowedHeader("Content-Type");
-        config.addAllowedHeader("Accept");
-        config.addAllowedHeader("Accept-Encoding");
-        config.addAllowedHeader("Accept-Language");
-        config.addAllowedHeader("Authorization");
-        config.addAllowedHeader("Auth-Token");
-        configSource.registerCorsConfiguration("/**",config);
-        return new CorsWebFilter(configSource);
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsWebFilter(source);
     }
 
 }
