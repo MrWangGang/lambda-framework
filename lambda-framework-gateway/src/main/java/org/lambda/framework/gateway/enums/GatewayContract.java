7ff5ee13da417e993033948fa4458ce947305dc0
wanggang <userbean@outlook.com>
2024-03-12 16:34:49 +0800

    更新token的时候加入token时间

--- lambda-framework-gateway/src/main/java/org/lambda/framework/gateway/enums/GatewayContract.java
+++ lambda-framework-gateway/src/main/java/org/lambda/framework/gateway/enums/GatewayContract.java
@@ -7,14 +7,14 @@ public class GatewayContract {
     public static final String HTTPS_SCHEME = "https";
     public static final String RSOCKET_MODEL = "RSocket-Model";
     public static final String RSOCKET_ECHO = "RSocket-Echo";
-    public static final String RSOCKET_MODEL_REQUEST_RESPONSE = "request-response";
-    public static final String RSOCKET_MODEL_REQUEST_STREAM = "request-stream";
-    public static final String RSOCKET_MODEL_FIRE_AND_FORGET = "fire-and-forget";
-    public static final String RSOCKET_MODEL_CHANNEL = "channel";
-    public static final String RSOCKET_ECHO_CHAR = "response-char";
-    public static final String RSOCKET_ECHO_ARRAY = "response-array";
-    public static final String RSOCKET_ECHO_OBJECT = "response-object";
-    public static final String RSOCKET_ECHO_MEDIA = "response-media";
-    public static final String RSOCKET_ECHO_VOID = "response-void";
+    public static final String RSOCKET_MODEL_REQUEST_RESPONSE = "request/response";
+    public static final String RSOCKET_MODEL_REQUEST_STREAM = "request/stream";
+    public static final String RSOCKET_MODEL_FIRE_AND_FORGET = "request/forget";
+    public static final String RSOCKET_MODEL_CHANNEL = "request/channel";
+    public static final String RSOCKET_ECHO_CHAR = "response/char";
+    public static final String RSOCKET_ECHO_ARRAY = "response/array";
+    public static final String RSOCKET_ECHO_OBJECT = "response/object";
+    public static final String RSOCKET_ECHO_MEDIA = "response/media";
+    public static final String RSOCKET_ECHO_VOID = "response/void";
 
 }
