72818a85afa57a3427c3a4afc8af6e4ec6bdf14b
wanggang <userbean@outlook.com>
2024-04-02 15:31:34 +0800

    让子类可以直接使用属性

--- lambda-framework-compliance/src/main/java/org/lambda/framework/compliance/repository/po/UnifyPO.java
+++ lambda-framework-compliance/src/main/java/org/lambda/framework/compliance/repository/po/UnifyPO.java
@@ -1,12 +1,18 @@
 package org.lambda.framework.compliance.repository.po;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UnifyPO<ID> implements Serializable {
     @JsonProperty
     protected ID id;
