736210b9b2369634bd1d75527c126b510af83efa
wanggang <userbean@outlook.com>
2024-04-23 15:51:51 +0800

    简化了common模块

--- lambda-framework-gateway/src/main/java/org/lambda/framework/gateway/filter/support/RsocketRequestFactory.java
+++ lambda-framework-gateway/src/main/java/org/lambda/framework/gateway/filter/support/RsocketRequestFactory.java
@@ -81,8 +81,13 @@ public class RsocketRequestFactory {
                             .flatMap(body-> {
                                 // 使用RSocket客户端发送请求
                                 RSocketRequester.RetrieveSpec retrieveSpec = null;
+                                if (MimeTypeUtils.APPLICATION_JSON.isCompatibleWith(contentType)) {
                                     retrieveSpec = requester
                                             .route(targetUri.getPath()).data(body);
+                                }else {
+                                    retrieveSpec = requester
+                                            .route(targetUri.getPath()).data(bodyFlux);
+                                }
 
                                 ServerHttpResponse rs = exchange.getResponse();
                                 switch (rsocketModel){
