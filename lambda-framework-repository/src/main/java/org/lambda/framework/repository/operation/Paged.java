e3e9851f4cd8e1c537f7f77bf4c142172675523f
wanggang <userbean@outlook.com>
2024-05-07 09:51:02 +0800

    简化了common模块

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/Paged.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/Paged.java
@@ -1,14 +1,14 @@
 package org.lambda.framework.repository.operation;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Paged<Entity> {
