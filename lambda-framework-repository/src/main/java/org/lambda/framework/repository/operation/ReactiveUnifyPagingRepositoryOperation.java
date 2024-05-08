dd1dd5562e599250223a86dfde10f1f5e4b298d3
wanggang <userbean@outlook.com>
2024-05-08 16:30:14 +0800

    对于分页查询使用并行处理的方式

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/ReactiveUnifyPagingRepositoryOperation.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/ReactiveUnifyPagingRepositoryOperation.java
@@ -8,7 +8,8 @@ import org.springframework.data.domain.Pageable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static java.lang.Math.ceil;
+import java.util.List;
+
 import static org.lambda.framework.repository.enums.RepositoryExceptionEnum.*;
 
 public interface ReactiveUnifyPagingRepositoryOperation<Entity> {
@@ -20,15 +21,22 @@ public interface ReactiveUnifyPagingRepositoryOperation<Entity> {
         Assert.verify(page.getPage(),ES_REPOSITORY_104);
         Assert.verify(page.getSize(),ES_REPOSITORY_105);
         if(page.getPage()<= 0 || page.getSize() <=0)throw new EventException(ES_REPOSITORY_100);
-        return operation.count(condition).switchIfEmpty(Mono.just(Long.valueOf(0))).flatMap(e->{
-            return Mono.just(Paged.builder().page(page.getPage()).size(page.getSize()).total(e).pages((long) ceil((double) e/(double) page.getSize())).build());
-        }).flatMap(e->{
-            //因为数据库的起点是从0开始，方法入参从1开始
-            Pageable pageable = PageRequest.of(page.getPage()-1,page.getSize());
-            return operation.query(pageable,condition).collectList().flatMap(records->{
-                e.setRecords(records);
-                return Mono.just(e);
-            });
-        });
+        //使用mono.zip执行并行处理，瓶颈来到了 i/o上。对于分页查询来说，这非常快。最后获得结果的时间由时间最长的线程处理决定
+        return Mono.zip(
+                        operation.count(condition).defaultIfEmpty(0L), // 计算 count
+                        operation.query(PageRequest.of(page.getPage() - 1, page.getSize()), condition).collectList() // 查询数据
+                )
+                .flatMap(tuple -> {
+                    Long count = tuple.getT1(); // 获取 count 结果
+                    List<?> records = tuple.getT2(); // 获取查询结果
+                    Paged paged = Paged.builder()
+                            .page(page.getPage())
+                            .size(page.getSize())
+                            .total(count)
+                            .pages((long) Math.ceil((double) count / (double) page.getSize()))
+                            .records(records)
+                            .build();
+                    return Mono.just(paged);
+                });
     }
 }
