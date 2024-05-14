b57de13351c58293abb595c200ab87985e5eb673
wanggang <userbean@outlook.com>
2024-05-14 21:38:49 +0800

    增加redisson分布式锁实现，对分布式锁进行增强，可以应用于响应式中

--- lambda-framework-lock/src/main/java/org/lambda/framework/lock/config/AbstractReactiveRedissonConfig.java
+++ lambda-framework-lock/src/main/java/org/lambda/framework/lock/config/AbstractReactiveRedissonConfig.java
@@ -1,7 +1,7 @@
 package org.lambda.framework.lock.config;
 
 import org.lambda.framework.common.exception.Assert;
-import org.lambda.framework.lock.ReactiveRedissonLockBuilder;
+import org.lambda.framework.lock.ReactiveRedissonBuilder;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -18,7 +18,7 @@ public abstract class AbstractReactiveRedissonConfig {
     //##数据库序号
     protected abstract Integer database();
 
-    protected ReactiveRedissonLockBuilder buildSingleClient(){
+    protected ReactiveRedissonBuilder buildSingleClient(){
         //密码不需要校验
         Assert.verify(this.host(),ES_LOCK_REDISSON_000);
         Assert.verify(this.port(),ES_LOCK_REDISSON_001);
@@ -30,6 +30,6 @@ public abstract class AbstractReactiveRedissonConfig {
                 .setDatabase(this.database())
                 .setPassword(password());
         RedissonClient redisson = Redisson.create(config);
-        return new ReactiveRedissonLockBuilder(redisson.reactive());
+        return new ReactiveRedissonBuilder(redisson.reactive());
     }
 }
