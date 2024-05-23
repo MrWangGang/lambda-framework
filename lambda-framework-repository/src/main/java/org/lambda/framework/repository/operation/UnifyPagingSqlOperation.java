e8b7850047a474d901f1be141bee35ce0acc4606
wanggang <userbean@outlook.com>
2024-05-23 21:09:17 +0800

    zk增加读写锁

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/UnifyPagingSqlOperation.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/UnifyPagingSqlOperation.java
@@ -1,9 +1,10 @@
 package org.lambda.framework.repository.operation;
 
+import org.springframework.data.domain.Pageable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UnifyPagingSqlOperation<Condition,VO> {
-    public Flux<VO> query(Condition condition);
+    public Flux<VO> query(Condition condition, Pageable pageable);
     public Mono<Long> count(Condition condition);
 }
