a88b827c20fa810f5627caa577e4117f3f51e059
wanggang <userbean@outlook.com>
2024-06-06 09:58:12 +0800

    更改了类加载逻辑

--- lambda-framework-rpc/src/main/java/org/lambda/framework/rpc/adapter/support/CustomClassLoader.java
+++ lambda-framework-rpc/src/main/java/org/lambda/framework/rpc/adapter/support/CustomClassLoader.java
@@ -11,8 +11,12 @@ import static org.lambda.framework.rpc.enums.RpcExceptionEnum.ES_RPC_018;
 
 public class CustomClassLoader  extends ClassLoader {
     private static final Logger logger = Logger.getLogger(LogFactory.class.getName());
-
+    public CustomClassLoader() {
+        // 将应用程序类加载器（系统类加载器）作为父加载器
+        super(CustomClassLoader.class.getClassLoader());
+    }
     public Class<?> loadClass(byte[] classBytes) throws ClassNotFoundException {
+        logger.info("java.classpath>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+System.getProperty("java.class.path"));
         logger.info("开始读取classBytes>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         InputStream inputStream = new ByteArrayInputStream(classBytes);
         byte[] buffer = new byte[classBytes.length];
