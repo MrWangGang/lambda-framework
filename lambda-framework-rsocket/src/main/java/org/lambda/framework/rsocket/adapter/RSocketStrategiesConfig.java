ae8005f58bbd6d3993203d9071707368c0c2fc3f
wanggang <userbean@outlook.com>
2024-06-14 15:27:39 +0800

    更改了类加载逻辑

---
+++ lambda-framework-rsocket/src/main/java/org/lambda/framework/rsocket/adapter/RSocketStrategiesConfig.java
@@ -0,0 +1,19 @@
+package org.lambda.framework.rsocket.adapter;
+
+import org.lambda.framework.common.util.sample.JsonUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+
+@Configuration
+public class RSocketStrategiesConfig {
+    @Bean
+    public RSocketStrategies customRSocketStrategies() {
+        return org.springframework.messaging.rsocket.RSocketStrategies.builder()
+                .decoder(new Jackson2JsonDecoder(JsonUtil.getJsonFactory()))
+                .encoder(new Jackson2JsonEncoder(JsonUtil.getJsonFactory()))
+                .build();
+    }
+}
