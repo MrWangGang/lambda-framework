2329b04e98511a097765ec981d80194a4ba42285
wanggang <userbean@outlook.com>
2024-06-29 20:19:07 +0800

    更改了类加载逻辑

--- lambda-framework-common/src/main/java/org/lambda/framework/common/po/SecurityLoginUser.java
+++ lambda-framework-common/src/main/java/org/lambda/framework/common/po/SecurityLoginUser.java
@@ -4,9 +4,5 @@ public interface SecurityLoginUser<ID> {
     //主键
 
     public ID getId();
-    public ID getOrganizationId();
-    public String getName();
     public void setId(ID id);
-    public void setOrganizationId(ID organizationId);
-    public void setName(String name);
 }
