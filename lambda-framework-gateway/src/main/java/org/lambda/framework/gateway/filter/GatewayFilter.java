24eafc3442f0baf1ceba4fae6aa760c381639325
wanggang <userbean@outlook.com>
2024-11-14 15:58:28 +0800

    mongodb自动创建索引

--- lambda-framework-gateway/src/main/java/org/lambda/framework/gateway/filter/GatewayFilter.java
+++ lambda-framework-gateway/src/main/java/org/lambda/framework/gateway/filter/GatewayFilter.java
@@ -7,6 +7,7 @@ import org.lambda.framework.compliance.security.SecurityPrincipalHolder;
 import org.lambda.framework.gateway.filter.support.RsocketRequestFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
@@ -16,7 +17,7 @@ import reactor.core.publisher.Mono;
 import java.net.URI;
 
 import static org.lambda.framework.gateway.enums.GatewayContract.*;
-import static org.lambda.framework.gateway.enums.GatewayExceptionEnum.*;
+import static org.lambda.framework.gateway.enums.GatewayExceptionEnum.ES_GATEWAY_000;
 
 @Component
 public class GatewayFilter implements GlobalFilter,Ordered{
@@ -34,15 +35,22 @@ public class GatewayFilter implements GlobalFilter,Ordered{
         Assert.verify(currentUri.getScheme(),ES_GATEWAY_000,"未知的请求协议");
         String _scheme = currentUri.getScheme();
         if(_scheme.equals(HTTP_SCHEME) || _scheme.equals(HTTPS_SCHEME)){
-            URI targetUri = (URI)exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+            Route route = (Route)exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+            if(route == null){
+                throw new EventException(ES_GATEWAY_000,"该资源没有对应的路由配置信息");
+            }
+            URI targetUri = route.getUri();
+            if(targetUri == null){
+                throw new EventException(ES_GATEWAY_000,"该资源没有对应的路由配置信息");
+            }
+
             if (RB_SCHEME.equals(targetUri.getScheme())) {
                 return rsocketRequestFactory.execute(exchange,chain,targetUri);
             }
-            /*if (LB_SCHEME.equals(targetUri.getScheme())) {
+            if (LB_SCHEME.equals(targetUri.getScheme())) {
                 return chain.filter(exchange);
             }
-            throw new EventException(ES_GATEWAY_000,"scheme仅支持rb与lb");*/
-            return chain.filter(exchange);
+            throw new EventException(ES_GATEWAY_000,"scheme仅支持rb与lb");
         }
         throw new EventException(ES_GATEWAY_000,"请求协议仅支持http与https");
     }
