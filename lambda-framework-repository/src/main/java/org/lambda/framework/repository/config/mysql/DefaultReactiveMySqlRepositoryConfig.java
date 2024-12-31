bbfbb20bca8f7e1f53e255a1e0bf29d879a91029
wanggang <userbean@outlook.com>
2024-12-31 15:11:43 +0800

    mongodb自动创建索引

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/config/mysql/DefaultReactiveMySqlRepositoryConfig.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/config/mysql/DefaultReactiveMySqlRepositoryConfig.java
@@ -1,12 +1,19 @@
 package org.lambda.framework.repository.config.mysql;
 
+import io.r2dbc.spi.ConnectionFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.lambda.framework.common.exception.EventException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
 import static org.lambda.framework.repository.enums.RepositoryExceptionEnum.*;
 
 public class DefaultReactiveMySqlRepositoryConfig  extends AbstractReactiveMySqlRepositoryConfig  {
+    @Bean
+    public ConnectionFactory mysql(){
+        return super.buildMysqlConnectionFactory();
+    }
+
     private String host;
 
     private String user;
