1ba4596fda6335b04f9b7b4fc24e54016da0a9a1
wanggang <userbean@outlook.com>
2025-01-03 17:39:58 +0800

    mongodb自动创建索引

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/elasticsearch/ReactiveElasticsearchCrudRepositoryOperation.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/operation/elasticsearch/ReactiveElasticsearchCrudRepositoryOperation.java
@@ -3,7 +3,8 @@ package org.lambda.framework.repository.operation.elasticsearch;
 
 import org.lambda.framework.repository.operation.ReactiveUnifyPagingRepositoryOperation;
 import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 
-public interface ReactiveElasticsearchCrudRepositoryOperation<Entity,IdType> extends ReactiveUnifyPagingRepositoryOperation<Entity>, ReactiveElasticsearchRepository<Entity, IdType> {
+public interface ReactiveElasticsearchCrudRepositoryOperation<Entity,IdType> extends ReactiveUnifyPagingRepositoryOperation<Entity>, ReactiveElasticsearchRepository<Entity, IdType> , ReactiveQueryByExampleExecutor<Entity> {
 
 }
