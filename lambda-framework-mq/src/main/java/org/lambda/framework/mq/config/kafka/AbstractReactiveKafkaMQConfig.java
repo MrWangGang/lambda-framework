82ec926f55af68ae06106141342e2dfb9989954d
wanggang <userbean@outlook.com>
2025-01-07 21:33:09 +0800

    repository根据配置类动态开启存储库功能,rpc启动模块根据启动类位置进行配置，不再使用注解

--- lambda-framework-mq/src/main/java/org/lambda/framework/mq/config/kafka/AbstractReactiveKafkaMQConfig.java
+++ lambda-framework-mq/src/main/java/org/lambda/framework/mq/config/kafka/AbstractReactiveKafkaMQConfig.java
@@ -28,7 +28,7 @@ public abstract class AbstractReactiveKafkaMQConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         SenderOptions<String, String> options = SenderOptions.create(props);
         return KafkaSender.create(options);
     }
