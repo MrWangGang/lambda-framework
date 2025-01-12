51d537620e6f826f9a84a9d8ba18964882aaec0b
wanggang <userbean@outlook.com>
2025-01-12 17:58:28 +0800

    es启动的时候创建索引

--- lambda-framework-repository/src/main/java/org/lambda/framework/repository/config/elasticsearch/AbstractReactiveElasticsearchRepositoryConfig.java
+++ lambda-framework-repository/src/main/java/org/lambda/framework/repository/config/elasticsearch/AbstractReactiveElasticsearchRepositoryConfig.java
@@ -1,15 +1,26 @@
 package org.lambda.framework.repository.config.elasticsearch;
 
+import jakarta.annotation.Resource;
+import org.apache.commons.logging.LogFactory;
 import org.lambda.framework.common.enums.ConverterEnum;
 import org.lambda.framework.common.exception.Assert;
 import org.lambda.framework.repository.config.converter.*;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ReactiveElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ReactiveElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
 import org.springframework.data.elasticsearch.core.convert.ElasticsearchCustomConversions;
+import org.springframework.data.elasticsearch.core.mapping.ElasticsearchPersistentEntity;
+import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 import static org.lambda.framework.repository.enums.RepositoryExceptionEnum.*;
 
@@ -51,5 +62,36 @@ public abstract class AbstractReactiveElasticsearchRepositoryConfig extends Reac
                 new LocalDateWriteConverter(),
                 new LocalDateTimeReadConverter(),
                 new LocalDateTimeWriteConverter()
-        ));    }
+        ));
+    }
+
+    private static final Logger logger = Logger.getLogger(LogFactory.class.getName());
+
+
+    @Resource
+    @Lazy
+    private ElasticsearchConverter elasticsearchConverter;
+
+    @Resource
+    @Lazy
+    private ReactiveElasticsearchTemplate reactiveElasticsearchTemplate;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initIndicesAfterStartup() {
+        logger.info("Elasticsearch InitIndicesAfterStartup init");
+        var init = System.currentTimeMillis();
+        var mappingContext = this.elasticsearchConverter.getMappingContext();
+        if (mappingContext instanceof SimpleElasticsearchMappingContext) {
+            SimpleElasticsearchMappingContext simpleElasticsearchMappingContext = (SimpleElasticsearchMappingContext) mappingContext;
+            for (ElasticsearchPersistentEntity<?> persistentEntity : simpleElasticsearchMappingContext.getPersistentEntities()) {
+                var clazz = persistentEntity.getType();
+                if (clazz.isAnnotationPresent(Document.class) &&!clazz.getSuperclass().isAnnotationPresent(Document.class)) {
+                    var indexOperations =reactiveElasticsearchTemplate.indexOps(clazz);  // 创建索引，如果已存在则不创建（可根据需求调整为更新等操作）
+                    var mappingFromEntity = indexOperations.createMapping();
+                        indexOperations.putMapping(mappingFromEntity).subscribe();
+                }
+            }
+        }
+        logger.info("Mongo InitIndicesAfterStartup take: {}"+(System.currentTimeMillis() - init));
+    }
 }
